d30b2bc164a2471b3bcd78be27b1a40ed56ec0af
Prakhyat Chaube <pxc0272@mavs.uta.edu>
2023-12-29 20:30:35 -0600

    Fourth Push

---
+++ arrays.js
@@ -0,0 +1,24 @@
+// var arr1 = ["Prakhyat", "Sushant", "Popular"];
+// var arr2 = arr1.splice(0, 1);
+// console.log(arr2);
+// var arr3 = new Array("Hi", "i am", "Prakhyat");
+// var arr4 = arr3.slice(0, 2);
+// console.log(arr4);
+
+// var arr5 = new Array("1", "2", "3", "4");
+// var arr6 = arr5.indexOf("1");
+// console.log(arr6);
+// arr5.unshift("8");
+// console.log(arr5);
+
+// var isEven = (element) => {
+//     return element % 2 === 0;
+// };
+// console.log(2);
+
+// function isEven(element) {
+//     if (element % 2 === 0)
+//         return true;
+// }
+// var result = [2, 3, 6, 8].every(isEven);
+// console.log(result);
---
+++ objects.js
@@ -0,0 +1,10 @@
+var employeeName = {
+    name: "Rick",
+    idNumber: 23,
+    salary: "$90000",
+    jobTitle: "Full Stack Developer",
+    experience: 3
+};
+console.log(employeeName.name);
+console.log(employeeName.experience);
+console.log(employeeName.jobTitle);
