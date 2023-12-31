a1682c4f7eea2db64983ded313fe49f580ed2acb
Prakhyat Chaube <pxc0272@mavs.uta.edu>
2023-12-31 02:29:09 -0600

    Seventh Push

---
+++ function1.js
@@ -0,0 +1,29 @@
+//Function Definition 
+
+//Making a typical function
+
+// function addition(num1, num2) {
+//     return num1 + num2;
+// }
+// console.log(addition(3, 4));
+
+// function multiplication(num1, num2, num3, num4) {
+//     return num1 * num2 * num3 * num4;
+// }
+
+// console.log(multiplication(4, 5, 6, 7));
+
+//Another type of function declaration
+// function userName(user) {
+//     return `${user} just logged in`;
+// }
+// console.log(userName("Prakash"));
+
+// function numOp(num1, num2) {
+//     if ((num1 && num2) === undefined) {
+//         return "Please enter valid nummbers";
+//     }
+//     return num1 + num2;
+// }
+
+// console.log(numOp(9, 4));
